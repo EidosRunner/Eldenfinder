bf54674e10e1144846e44d6ff43e5295d244524b
shadow7381606 <clinetchase@gmail.com>
2026-03-31 12:57:58 +0200

    start html

---
+++ contact page/contact.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <h1>Formulaire de contact</h1>
+        <nav>
+            <ul>
+                <li>
+                    <a href="/welcome">Welcome</a>
+                </li>
+                <li>
+                    <a href="/catalog">Catalog</a>
+                </li>
+                <li>
+                    <a href="/contact">Contact</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+         <form action="/contact">
+        <label for="pseudo">pseudo</label>
+        <input type="text" name="pseudo" id="pseudo">
+        
+        <label for="message">Parlez</label>
+        <textarea name="message" id="message"></textarea>
+ 
+        <button type="submit">Envoyer</button>
+    </form>
+    <figure>
+        <img src="#" alt="map">
+    </figure>
+    </main>
+   
+<footer>
+     <address>
+        <span>eldenFinder</span>
+        <span>EldenFinder@gmail.com</span>
+    </address>
+   
+</footer>
+   
+</body>
+</html>
+ 
