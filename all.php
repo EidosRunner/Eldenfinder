90a75ef6a5bc3bbf438fb8bbd13562bace96008b
shadow7381606 <clinetchase@gmail.com>
2026-05-19 10:14:12 +0200

    fixed duplicated things

---
+++ all.php
@@ -0,0 +1,3 @@
+<?php include'../other_page/top.php';
+
+include'../other_page/bottom.php';?>
---
+++ other_page/bottom.php
@@ -0,0 +1,10 @@
+
+<footer>
+<address>
+    <span>EldenFinder</span>
+    <span>EldenFinder@gmail.com</span>
+</address>
+
+&#xA9; <?php echo date('Y')?>
+
+</footer>
