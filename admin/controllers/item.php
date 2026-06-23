09035070428ea95eb27e921d4a7c17369f2ffe19
EidosRunner <clinetchase@gmail.com>
2026-06-23 10:43:18 +0200

    changed admin item.php

--- admin/controllers/item.php
+++ admin/controllers/item.php
@@ -1,13 +1,7 @@
 <?php
 require __DIR__.'/../models/catalog.php';
 
-function admin_index($pdo)
-{
-    $content = render(__DIR__ . '/../views/dashboard.php', []);
-    return render(__DIR__ . '/../views/_layout.php', ['page_content' => $content, 'page_title' => 'Dashboard']);
-}
-
-function admin_item($pdo)
+function item_index($pdo)
 {
     $items = get_all_items_admin($pdo);
     $content = render(__DIR__ . '/../views/items.php', ['items' => $items]);
